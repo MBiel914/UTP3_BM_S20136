9c409118ee70c492e656f0d1cc46d66ff4d4887b
MBiel914 <michalbielecki.914@o2.pl>
2021-04-21 19:20:43 +0200

    Zad 2 Ready

--- src/zad2/CustomersPurchaseSortFind.java
+++ src/zad2/CustomersPurchaseSortFind.java
@@ -6,6 +6,70 @@
 
 package zad2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class CustomersPurchaseSortFind {
+	private List<Purchase> _purchaseList = null;
+	private File _file =  null; 
+	private Scanner _scanner = null;
+	private Purchase _purchase = null;
+	
+	public void readFile(String fname) {
+		_purchaseList = new ArrayList<Purchase>();
+		try {
+			_file = new File(fname);
+			_scanner = new Scanner(_file);
+			
+			while (_scanner.hasNextLine())
+			{
+				String[] splited = _scanner.nextLine().split(";| ");
+				
+				_purchase = new Purchase(splited[0],
+											splited[1], 
+											splited[2], 
+											splited[3], 
+											Double.parseDouble(splited[4]), 
+											Double.parseDouble(splited[5]),
+											Double.parseDouble(splited[4]) * Double.parseDouble(splited[5])
+											);
+			 
+				_purchaseList.add(_purchase); 
+			} 
+		} catch (FileNotFoundException ex) {
+			System.err.println("Read file error: " + ex.getMessage());
+		}	
+	}
+
+	public void showSortedBy(String sortType) {
+		Comparator<Purchase> sortByNazwisko = Comparator.comparing(Purchase::getNazwisko).thenComparing(Purchase::getId_klienta);
+		Comparator<Purchase> sortByKoszt = Comparator.comparingDouble(Purchase::getKoszt).reversed().thenComparing(Purchase::getId_klienta);
+		
+		switch (sortType) {
+			case "Nazwiska":
+				System.out.println(sortType);
+				
+				_purchaseList.stream().sorted(sortByNazwisko).forEach(System.out::println);
+				break;
+	
+			case "Koszty":
+				System.out.println(sortType);
+				
+				_purchaseList.stream().sorted(sortByKoszt).forEach(e -> System.out.println(e + " (koszt: " + e.koszt + ")"));
+				break;
+		}
+
+		System.out.println();
+	}
+
+	public void showPurchaseFor(String id) {
+		System.out.println("Klient " + id);
+		_purchaseList.stream().filter(e -> id.equals(e.getId_klienta())).forEach(System.out::println);
+		
+		System.out.println();
+	}
 }
--- src/zad2/Purchase.java
+++ src/zad2/Purchase.java
@@ -8,4 +8,38 @@ package zad2;
 
 
 public class Purchase {
+	String id;
+	String nazwisko;
+	String imie;
+	String nazwaTw;
+	double cena;
+	double ilosc;
+	double koszt;
+
+	public Purchase(String id,String nazwisko, String imie, String nazwaTw, double cena, double ilosc, double koszt) {
+		this.id = id;
+		this.nazwisko = nazwisko;
+		this.imie = imie;
+		this.nazwaTw = nazwaTw;
+		this.cena = cena;
+		this.ilosc = ilosc;
+		this.koszt = koszt;
+	}
+	
+	public String getId_klienta() {
+		return id;
+	}
+
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	
+	public double getKoszt() {
+		return koszt;
+	}
+	
+	@Override
+	public String toString() {
+		return id + ";" + nazwisko + " " + imie + ";" + nazwaTw + ";" + cena + ";" + ilosc;
+	}
 }
